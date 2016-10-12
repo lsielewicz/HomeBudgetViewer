80c0b181ecad1110b1e6b2c542d6d0fe2eb4eeef
lsielewicz <lsielewicz8@gmail.com>
2016-10-12 20:51:14 +0200

    Added mvvm-light and template-10 frameworks

---
+++ HomeBudgetViewer/HomeBudgetViewer/Presentation/AppViewModelBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Navigation;
+using GalaSoft.MvvmLight;
+using Template10.Common;
+using Template10.Services.NavigationService;
+
+namespace HomeBudgetViewer.Presentation
+{
+    public abstract class AppViewModelBase : ViewModelBase, INavigable
+    {
+        public Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> state)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task OnNavigatedFromAsync(IDictionary<string, object> suspensionState, bool suspending)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task OnNavigatingFromAsync(NavigatingEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+
+        public INavigationService NavigationService { get; set; }
+        public IDispatcherWrapper Dispatcher { get; set; }
+        public IStateItems SessionState { get; set; }
+
+        private bool _isLoading;
+
+        public bool IsLoading
+        {
+            get
+            {
+                return this._isLoading;
+            }
+            set
+            {
+                if (this._isLoading == value)
+                    return;
+                this._isLoading = value;
+                this.RaisePropertyChanged();
+            }
+        }
+    }
+}
---
+++ HomeBudgetViewer/HomeBudgetViewer/Presentation/ViewModelLocator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using HomeBudgetViewer.Presentation.MainPage;
+using Microsoft.Practices.ServiceLocation;
+
+namespace HomeBudgetViewer.Presentation
+{
+    public class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(()=>SimpleIoc.Default);
+            if (ViewModelBase.IsInDesignModeStatic)
+            {
+
+            }
+            else
+            {
+               
+            }
+
+            SimpleIoc.Default.Register<MainPageViewModel>();
+        }
+
+        public MainPageViewModel MainPageViewModel
+        {
+            get { return ServiceLocator.Current.GetInstance<MainPageViewModel>(); }
+        }
+
+    }
+}
