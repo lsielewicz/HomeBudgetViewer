1faf3990490e78aa77d849ef8888e53690cd5003
lsielewicz <lsielewicz8@gmail.com>
2016-10-20 13:10:36 +0200

    Overview page - part 6

---
+++ HomeBudgetViewer/HomeBudgetViewer/Conventers/CoutOfCollectionItemsToVisibilityConventer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using HomeBudgetViewer.Models.Enum;
+
+namespace HomeBudgetViewer.Conventers
+{
+    public class CoutOfCollectionItemsToVisibilityConventer : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value != null && value is int)
+            {
+                var intValue = (int)value;
+                if (parameter != null && (ConventerDirection)parameter == ConventerDirection.Inverse)
+                {
+                    if (intValue == 0)
+                        return Visibility.Visible;
+                    return Visibility.Collapsed;
+                }
+                if (intValue == 0)
+                    return Visibility.Collapsed;
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
