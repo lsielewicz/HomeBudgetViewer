cecd18ca002c13e5df2b5cd8ecd50edb7dd4d20d
lsielewicz <lsielewicz8@gmail.com>
2016-10-22 19:37:29 +0200

    Added adaptive triggers to overview page

--- HomeBudgetViewer/HomeBudgetViewer/Controls/NoActivitiesTextControl/NoActivitiesTextControl.xaml.cs
+++ HomeBudgetViewer/HomeBudgetViewer/Controls/NoActivitiesTextControl/NoActivitiesTextControl.xaml.cs
@@ -19,6 +19,36 @@ namespace HomeBudgetViewer.Controls.NoActivitiesTextControl
 {
     public sealed partial class NoActivitiesTextControl : UserControl
     {
+        public static DependencyProperty ControlImageLengthProperty = DependencyProperty.Register(
+            "ControlImageLength",
+            typeof(double), 
+            typeof(NoActivitiesTextControl), 
+            new PropertyMetadata(100.0));
+
+        public static DependencyProperty ControlTextSizeProperty = DependencyProperty.Register(
+            "ControlTextSize",
+            typeof(double),
+            typeof(NoActivitiesTextControl),
+            new PropertyMetadata(14.0));
+
+        public double ControlImageLenght
+        {
+            get { return (double) this.GetValue(ControlImageLengthProperty); }
+            set
+            {
+                this.SetValue(ControlImageLengthProperty,value);
+            }
+        }
+
+        public double ControlTextSize
+        {
+            get { return (double) this.GetValue(ControlTextSizeProperty); }
+            set
+            {
+                this.SetValue(ControlTextSizeProperty,value);
+            }
+        }
+
         public NoActivitiesTextControl()
         {
             this.InitializeComponent();
