57871863c4d18ad2928e6019b452add5b57bc0c2
lsielewicz <lsielewicz8@gmail.com>
2016-12-07 18:34:12 +0100

    Categories fix

---
+++ HomeBudgetViewer/HomeBudgetViewer/Helpers_/CategoriesHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
+using HomeBudgetViewer.Database.Engine.Restrictions.Categories;
+
+namespace HomeBudgetViewer.Helpers_
+{
+    public class CategoriesHelper
+    {
+        private static CategoriesHelper _instance;
+        public static CategoriesHelper Instance
+        {
+            get
+            {
+                return _instance ?? (_instance = new CategoriesHelper());
+            }
+        }
+
+
+        private readonly ResourceLoader _resourceLoader;
+        private CategoriesHelper()
+        {
+            _resourceLoader = ResourceLoader.GetForViewIndependentUse();
+        }
+
+        private string GetLocalizedString(string resourceKey)
+        {
+            return this._resourceLoader.GetString(resourceKey);
+        }
+
+        public  List<CategoryModel> LocalizedCategories
+        {
+            get
+            {
+                List<CategoryModel> localizedCategories = new List<CategoryModel>();
+                CategoryModel.PossibleCategories.ForEach(
+                        category =>
+                        {
+                            localizedCategories.Add(new CategoryModel()
+                            {
+                                CategoryName = this.GetLocalizedString($"{category.CategoryName}"),
+                                CategoryEnum = category.CategoryEnum,
+                                ItemType = category.ItemType
+                            });
+                        });
+
+                return localizedCategories;
+            }
+        }
+    }
+}
